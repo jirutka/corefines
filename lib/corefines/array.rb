08b2d16702f321ada51d978de20a161e8b592b5e
Jakub Jirutka <jakub@jirutka.cz>
2015-01-31 14:33:04 +0100

    Add Array#index_by

--- lib/corefines/array.rb
+++ lib/corefines/array.rb
@@ -2,6 +2,47 @@ require 'corefines/support/alias_submodules'
 
 module Corefines
   module Array
+
+    ##
+    # @!method index_by
+    #   Convert array into a Hash, iterating over each element where the
+    #   provided block must return the key to be used to map to the value.
+    #
+    #   It's similar to {::Enumerable#group_by}, but when two elements
+    #   corresponds to the same key, then only the last one is preserved in the
+    #   resulting Hash.
+    #
+    #   @example
+    #     people.index_by(&:login)
+    #     => { "flynn" => <Person @login="flynn">, "bradley" => <Person @login="bradley">, ...}
+    #
+    #   @example
+    #     people.index_by.each(&:login)
+    #     => { "flynn" => <Person @login="flynn">, "bradley" => <Person @login="bradley">, ...}
+    #
+    #   @overload index_by
+    #     @return [Enumerator]
+    #
+    #   @overload index_by
+    #     @yield [obj] gives each array's element to the block.
+    #     @yieldreturn the key to be used to map to the value.
+    #     @return [Hash]
+    #
+    module IndexBy
+      refine ::Array do
+        def index_by
+          if block_given?
+            ::Hash[map { |elem| [ yield(elem), elem ] }]
+          else
+            # Why can't use #enum_for here, because refinement is not active in this context.
+            ::Enumerator.new(size) do |y|
+              ::Hash[map { |elem| [ y.yield(elem), elem ] }]
+            end
+          end
+        end
+      end
+    end
+
     ##
     # @!method map_send(method_name, *args, &block)
     #   Sends a message to each element and collects the result.
---
+++ spec/array/index_by_spec.rb
@@ -0,0 +1,26 @@
+describe Array do
+  using Corefines::Array::index_by
+
+  describe '#index_by' do
+
+    let(:input) { %w[alpha bee beta gamma] }
+    let(:expected) { {a: 'alpha', b: 'beta', g: 'gamma'} }
+
+    context "with block" do
+      it "returns hash with the array's elements keyed be result of the block" do
+        expect(input.index_by { |s| s[0].to_sym }).to eq expected
+      end
+    end
+
+    context "without block" do
+      it "returns enumerator that behaves the same as when block is given" do
+        expect(input.index_by).to be_instance_of Enumerator
+        expect(input.index_by.each { |x| x[0].to_sym }).to eq expected
+      end
+
+      it "returns enumerator that knows its size" do
+        expect(input.index_by.size).to eq input.size
+      end
+    end
+  end
+end
