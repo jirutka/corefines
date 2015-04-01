14f0478e92d4e9c2e1f0c76105850bb5310f3421
Jakub Jirutka <jakub@jirutka.cz>
2015-04-01 23:32:25 +0200

    Remove Symbol::~
    I still can't decide whether to use ~ as an alias for #to_proc (for use
    in case statements), or for #method. So it'll be better to remove it for
    now.

--- lib/corefines/symbol.rb
+++ lib/corefines/symbol.rb
@@ -27,28 +27,6 @@ module Corefines
       end
     end
 
-    ##
-    # @!method ~
-    #   Alias for +#to_proc+.
-    #
-    #   @example
-    #     case num
-    #     when ~:zero?
-    #       ...
-    #     when ~:odd?
-    #       ...
-    #     when ~:even?
-    #       ...
-    #     end
-    #
-    #  @return [Proc] a Proc object which respond to the given method by +sym+.
-    #
-    module OpTilde
-      refine ::Symbol do
-        alias_method :~, :to_proc
-      end
-    end
-
     include Support::AliasSubmodules
   end
 end
