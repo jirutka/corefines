0f0a5505e1546a5bc8bafc0bb69455a7aa097c7c
Jakub Jirutka <jakub@jirutka.cz>
2015-07-20 01:38:12 +0200

    Add Module#autocurry

--- lib/corefines/module.rb
+++ lib/corefines/module.rb
@@ -90,6 +90,80 @@ module Corefines
       end
     end
 
+    ##
+    # @!method autocurry(*meth_names)
+    #   Makes the specified methods auto-currying.
+    #   If used with no parameters, subsequently defined methods become
+    #   auto-currying.
+    #
+    #   @example Autocurry specified methods:
+    #     class Foo
+    #       def add(x, y, z)
+    #         x + y + z
+    #       end
+    #       autocurry :add
+    #     end
+    #
+    #   @example Autocurry all subsequently defined methods:
+    #     class Foo
+    #       autocurry
+    #       def add(x, y, z)
+    #         x + y + z
+    #       end
+    #     end
+    #
+    #   @example
+    #     obj = Foo.new
+    #
+    #     obj.add(1, 2, 3)   # => 6
+    #     obj.add(1)         # => #<Proc:0x123 (lambda)>
+    #     obj.add(1).(2).(3) # => 6
+    #     obj.add(1, 2).(3)  # => 6
+    #     obj.add(1).(2, 3)  # => 6
+    #
+    #   @param *meth_names [Symbol] names of the methods to be autocurried.
+    #   @return [self]
+    #
+    module Autocurry
+      refine ::Module do
+        def autocurry(*method_names)
+          if method_names.empty?
+            autocurry_subsequent
+          else
+            method_names.each { |name| autocurry_method(name) }
+          end
+          self
+        end
+
+        private
+
+        def autocurry_method(name)
+          orig_meth = instance_method(name)
+
+          define_method(name) do |*args|
+            orig_meth.bind(self).to_proc.curry.call(*args)
+          end
+        end
+
+        # XXX is this thread safe?
+        def autocurry_subsequent
+          in_use = nil
+
+          if singleton_class.method_defined? :method_added
+            orig_method_added = method(:method_added)
+          end
+
+          define_singleton_method(:method_added) do |meth_name|
+            return if in_use
+            in_use = true
+            autocurry_method(meth_name)
+            in_use = false
+            orig_method_added.call(meth_name) if orig_method_added
+          end
+        end
+      end
+    end
+
     include Support::AliasSubmodules
   end
 end
---
+++ spec/module/autocurry_spec.rb
@@ -0,0 +1,120 @@
+describe Module do
+  using Corefines::Module::autocurry
+
+  describe '#autocurry' do
+
+    subject(:obj) { klass.new }
+
+    context "without arguments" do
+
+      let!(:klass) do
+        Class.new do
+          def self.method_added(name)
+            (@called ||= []) << name
+          end
+
+          def alpha(x) x end
+
+          autocurry
+          def beta(x) x end
+          def gamma(x) x end
+        end
+      end
+
+      it "curries all methods after autocurry call" do
+        expect( obj.beta ).to be_a Proc
+        expect( obj.gamma ).to be_a Proc
+      end
+
+      it "does not autocurry methods before autocurry call" do
+        expect { obj.alpha }.to raise_error ArgumentError
+      end
+
+      it "does not override existing #method_added" do
+        expect( klass.instance_variable_get(:@called) ).to eq [:alpha, :beta, :gamma]
+      end
+    end
+
+    context "with a single argument" do
+
+      let(:klass) do
+        Class.new do
+          def alpha(x) x end
+          autocurry :alpha
+          def beta(x) x end
+          def gamma(x) x end
+          autocurry :gamma
+        end
+      end
+
+      it "curries the specified method" do
+        expect( obj.alpha ).to be_a Proc
+        expect( obj.gamma ).to be_a Proc
+      end
+
+      it "does not autocurry other methods" do
+        expect { obj.beta }.to raise_error ArgumentError
+      end
+    end
+
+    context "with multiple arguments" do
+
+      let(:klass) do
+        Class.new do
+          def alpha(x) x end
+          def beta(x) x end
+          autocurry :alpha, :beta
+          def gamma(x) x end
+        end
+      end
+
+      it "curries the specified methods" do
+        expect( obj.alpha ).to be_a Proc
+        expect( obj.beta ).to be_a Proc
+      end
+
+      it "does not autocurry other methods" do
+        expect { obj.gamma }.to raise_error ArgumentError
+      end
+    end
+
+
+    describe "currying behaviour" do
+
+      let(:klass) do
+        Class.new do
+          autocurry
+          def add(x, y, z)
+            x + y + z
+          end
+        end
+      end
+
+      context "when all parameters are provided" do
+        it "invokes method normally" do
+          expect( obj.add(1, 2, 3) ).to eq 6
+        end
+      end
+
+      context "when less than required parameters are given" do
+        it "returns a curried Proc" do
+          expect( obj.add ).to be_a Proc
+          expect( obj.add(1) ).to be_a Proc
+          expect( obj.add(1, 2) ).to be_a Proc
+        end
+      end
+
+      context "when required parameters are provided successively" do
+        it "invokes curried Proc after giving the last parameter" do
+          expect( obj.add(1).(2).(3) ).to eq 6
+          expect( obj.add(1).(2, 3) ).to eq 6
+          expect( obj.add(1, 2).(3) ).to eq 6
+        end
+      end
+
+      context "when too many args passed to curried Proc" do
+        it { expect { obj.add(1, 2).(3, 4) }.to raise_error ArgumentError }
+      end
+    end
+  end
+end
